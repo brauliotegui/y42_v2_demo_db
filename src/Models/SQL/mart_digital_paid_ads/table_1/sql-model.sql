16cec147c0463719b15d4d5f11610da6c2d0a1ae
Alfred Stehli <alfred.stehli@y42.com>
2022-10-07 13:14:39 +0200

    models: update model "mart_digital_paid_ads"

--- src/Models/SQL/mart_digital_paid_ads/table_1/sql-model.sql
+++ src/Models/SQL/mart_digital_paid_ads/table_1/sql-model.sql
@@ -6,8 +6,8 @@ Select
 from {{ ref('src/Models/UI/stg_google_ads/stg_google_ads') }}
 UNION ALL
 Select 
+    `date`,
   `campaign_name`,
-  `spend`,
   `ad_source`,
-  `date`
+  `spend`
 from {{ ref('src/Models/UI/stg_fb_ads/stg_fb_ads') }}
