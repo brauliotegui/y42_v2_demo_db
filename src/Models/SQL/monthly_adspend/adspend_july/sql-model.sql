118cedf37aea6dd589ad004650af6f7981a0f5d7
Johannes Pittgens <johannes.pittgens@y42.com>
2022-11-01 00:26:20 +0100

    adding multiple models

--- src/Models/SQL/monthly_adspend/adspend_july/sql-model.sql
+++ src/Models/SQL/monthly_adspend/adspend_july/sql-model.sql
@@ -1,6 +1,6 @@
 SELECT 
-  `date`,
-  `last_click_channel`,
-  `adspend`
+  `date` AS Date,
+  `last_click_channel` AS Attributed_Channel,
+  `adspend` AS Spend
 FROM {{ ref('src/Models/UI/ads_spend_combined/ads_spend_combined') }}
 WHERE `date` >= '2020-07-01' AND `date` < '2020-08-01'
